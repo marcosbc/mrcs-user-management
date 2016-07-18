84f1cfc56741fa9c69b7380cf34748b1322683e7
Marcos Bjoerkelund <marcosbjorkelund@gmail.com>
2016-07-18 22:38:09 +0200

    Change default NodeJS port in example configuration file

--- conf/configuration.example.js
+++ conf/configuration.example.js
@@ -3,12 +3,12 @@ const config = {
   // Path where the API will be accessed from the browser
   base: '/',
   // Port which the API server will run on
-  port: 4000,
+  port: 3000,
   // Front-end settings, for obtaining URLs
   frontend: {
     protocol: 'http',
     host: '127.0.0.1',
-    port: 4000
+    port: 3000
   },
   // Security-related
   security: {
